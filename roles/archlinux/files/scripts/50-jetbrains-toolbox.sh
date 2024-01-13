ff7fa26db29d8ec482ca9a83cf3f117461c9da96
Jener Rasmussen <jener@jener.dk>
2024-01-13 12:31:57 +0100

    Improvement

---
+++ roles/archlinux/files/scripts/50-jetbrains-toolbox.sh
@@ -0,0 +1,1 @@
+export PATH="$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts"
---
+++ roles/common/files/50-bgnotify.sh
@@ -0,0 +1,4 @@
+# Configuration for oh-my-zsh plugin bgnotify
+# https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/bgnotify
+bgnotify_bell=true
+bgnotify_threshold=30
